868b8b8d9ae876d94afbb6bc602b6945a4ca8f99
eli <e.f05484293@gmail.com>
2022-12-13 21:32:11 +0200

    Bug fixed

---
+++ Controlers/ErrorControler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulls_and_Cows
+{
+    internal class ErrorControler
+    {
+        ErrorsClass ErrorsClass = new ErrorsClass();
+        public ErrorsModel ErrorsModel(string Number)
+        {
+            ErrorsModel errorsModel = new ErrorsModel();
+
+            // Here The numbers of the input is testd
+            //bool flag = true;
+            
+            if (ErrorsClass.isit4digits(Number) == true) { errorsModel.Posibile = true; }
+            else
+            {
+                errorsModel.Posibile = false;
+                errorsModel.MessageError = " Error !: Enter exactly 4 numbers ";
+                return errorsModel;
+            }
+
+            if (ErrorsClass.isTheFirstDigit0(Number) == true) { errorsModel.Posibile = true; }
+            else
+            {
+                errorsModel.Posibile = false;
+                errorsModel.MessageError = " Error !: The first digit most not be '0' ";
+                return errorsModel;
+            }
+
+            if (ErrorsClass.isTwoNumbersAreTheSame(Number) == true) { errorsModel.Posibile = true; }
+            else
+            {
+                errorsModel.Posibile = false;
+                errorsModel.MessageError = " Error !: Two digits cannot be the same  ";
+                return errorsModel;
+            }
+            return errorsModel;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp3;
+using ConsoleApp3.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulls_and_Cows
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            ErrorControler errorControler = new ErrorControler();
+            ErrorsModel errorsModel = new ErrorsModel();
+            ChackModel chackModel = new ChackModel();
+            ChackClass chackClass = new ChackClass();
+            CreateRandNumControler createRandNumControler = new CreateRandNumControler();
+
+            string secretNumber = createRandNumControler.SecretNum();
+            int Guess = 0;
+            while (Guess < 12)
+            {
+                
+                Console.WriteLine("Enter a number of 4 digits ");
+
+                string inputNumber = Console.ReadLine();
+                //Checking whether the entered number meets the conditions.
+                errorsModel = errorControler.ErrorsModel(inputNumber);
+                if (errorsModel.Posibile == true)
+                {
+                    chackModel = chackClass.chackModel(inputNumber, secretNumber);
+                    Console.WriteLine("You Have " + chackModel.cows + " Cows and " + chackModel.bulls + " Bulls");
+                    Guess++;
+                }
+                else
+                {
+                    Console.WriteLine(errorsModel.MessageError);
+                }
+                //bool isIt4Digits = errors.isit4digits(input);
+            }
+
+
+        }
+    }
+}
